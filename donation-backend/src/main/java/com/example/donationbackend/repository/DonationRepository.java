6c4178ca6d30fffa4a22e9f7686eb687d6698173
2400030817-DDR <dharanidarreddychidipudi@gmail.com>
2026-04-08 09:03:27 +0530

    Delete donation-backend directory

--- donation-backend/src/main/java/com/example/donationbackend/repository/DonationRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.donationbackend.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.donationbackend.entity.Donation;
-
-public interface DonationRepository extends JpaRepository<Donation, Long> {
-}
